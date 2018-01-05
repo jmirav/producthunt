eef201a866a1749d2b43a45265ca218f25d1525c
jmirav <jmira420@gmail.com>
2018-01-05 11:35:15 -0500

    Implementa la funcionalidad de registrar usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
+  resources :products
+
+  root 'products#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/products', to: 'products#index'
   # get '/products/new', to: 'products#new', as: 'new_product'
@@ -7,8 +13,4 @@ Rails.application.routes.draw do
   # get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   # patch '/products/:id', to: 'products#update'
   # delete '/products/:id', to: 'products#destroy'
-
-  resources :products
-
-  root 'products#index'
 end
