7ef6f758374a99400b3251f846a5f5dee1691228
jmirav <jmira420@gmail.com>
2018-01-13 13:16:20 -0500

    Ajusta configuración de AWS en dev y prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,13 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_region: ENV.fetch('AWS_REGION'),
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
+
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
 end
