754df63ba42f73be61b5f928d202eb112d49a2e2
jmirav <jmira420@gmail.com>
2018-01-13 13:19:28 -0500

    Ajusta configuración de AWS en dev y prod 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,15 +52,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: ENV['AWS_REGION']
-    }
-  }
-
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV['S3_BUCKET_NAME'],
+  #     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  #     s3_region: ENV['AWS_REGION']
+  #   }
+  # }
+  #
+  # Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
 end
